972259cba035edec5df2ca02b1dd8899b8bf729d
Sidhant Kumar <84363346+Trazer09@users.noreply.github.com>
2025-01-14 13:58:35 +0530

    Added to github

--- src/components/other/CreateTask.jsx
+++ src/components/other/CreateTask.jsx
@@ -14,28 +14,37 @@ const CreateTask = () => {
     const [newTask, setNewTask] = useState({})
 
     const submitHandler = (e) => {
-        e.preventDefault()
-
-        setNewTask({ taskTitle, taskDescription, taskDate, category, active: false, newTask: true, failed: false, completed: false })
-
-        const data = userData
-
+        e.preventDefault();
+    
+        const newTask = {
+          taskTitle,
+          taskDescription,
+          taskDate,
+          category,
+          active: false,
+          newTask: true,
+          failed: false,
+          completed: false,
+        };
+    
+        const data = userData;
+    
         data.forEach(function (elem) {
-            if (asignTo == elem.firstName) {
-                elem.tasks.push(newTask)
-                elem.taskCounts.newTask = elem.taskCounts.newTask + 1
-            }
-        })
-        setUserData(data)
+          if (asignTo === elem.firstName) {
+            elem.tasks.push(newTask);
+            elem.taskCounts.newTask = elem.taskCounts.newTask + 1;
+          }
+        });
+        setUserData(data);
         console.log(data);
-
-        setTaskTitle('')
-        setCategory('')
-        setAsignTo('')
-        setTaskDate('')
-        setTaskDescription('')
-
-    }
+    
+        setTaskTitle("");
+        setCategory("");
+        setAsignTo("");
+        setTaskDate("");
+        setTaskDescription("");
+      };
+    
 
     return (
         <div className='p-5 bg-[#1c1c1c] mt-5 rounded'>
